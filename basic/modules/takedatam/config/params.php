81d6d945780f02167ed68ec9ac8efd70a0678e3c
EgorVerh <76529351+EgorVerh@users.noreply.github.com>
2024-04-11 14:44:02 +0300

    Add files via upload

--- basic/modules/takedatam/config/params.php
+++ basic/modules/takedatam/config/params.php
@@ -1,5 +1,7 @@
 <?php
 return [
-    'adminEmail' => 'my',
-
+    'key' => 'minioadmin',
+    'secret' => 'minioadmin',
+    'Bucket' => 'testbucket',
+    'endpoint' => 'http://192.168.33.23:9000',//192.168.33.23:9000
 ];
