1e676058b19d2cd608a1bbd20a329ad6c9fc7840
EgorVerh <76529351+EgorVerh@users.noreply.github.com>
2024-08-06 15:35:15 +0300

    Add files via upload

---
+++ basic/modules/takedatam/migrations/m240805_135628_Relations.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m240805_135628_Relations extends Migration
+{
+    public function safeUp()
+    {
+        $this->addForeignKey('fk_savefiles_fieldsforms_id',
+            '{{%savefiles}}','fieldsforms_id',
+            '{{%fieldsforms}}','id',
+            'CASCADE','CASCADE'
+         );
+    }
+
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_savefiles_fieldsforms_id', '{{%savefiles}}');
+    }
+}
