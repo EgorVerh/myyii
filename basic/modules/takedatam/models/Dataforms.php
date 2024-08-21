1226425138c60893655f78f1c34b163b8c748d87
EgorVerh <76529351+EgorVerh@users.noreply.github.com>
2024-08-21 16:29:22 +0300

    Add files via upload

---
+++ basic/modules/takedatam/models/Dataforms.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "dataforms".
+ *
+ * @property int $id
+ * @property string $titel
+ * @property string|null $data
+ * @property int $fieldsforms_id
+ * @property int $enabled
+ * @property string|null $position
+ * @property string $created_at
+ * @property string|null $updated_at
+ *
+ * @property ExtraFields[] $extraFields
+ * @property Fieldsforms $fieldsforms
+ */
+class Dataforms extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'dataforms';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['titel', 'fieldsforms_id'], 'required'],
+            [['titel', 'data', 'position'], 'string'],
+            [['fieldsforms_id', 'enabled'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['fieldsforms_id'], 'exist', 'skipOnError' => true, 'targetClass' => Fieldsforms::class, 'targetAttribute' => ['fieldsforms_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'titel' => 'Titel',
+            'data' => 'Data',
+            'fieldsforms_id' => 'Fieldsforms ID',
+            'enabled' => 'Enabled',
+            'position' => 'Position',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[ExtraFields]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getExtraFields()
+    {
+        return $this->hasMany(ExtraFields::class, ['dataforms_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Fieldsforms]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFieldsforms()
+    {
+        return $this->hasOne(Fieldsforms::class, ['id' => 'fieldsforms_id']);
+    }
+}
---
+++ basic/modules/takedatam/models/Fieldsforms.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "fieldsforms".
+ *
+ * @property int $id
+ * @property string $nameform
+ * @property string $fieldform
+ * @property int $count_upload_doc
+ *
+ * @property Dataforms[] $dataforms
+ * @property ExtraFields[] $extraFields
+ */
+class Fieldsforms extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'fieldsforms';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nameform', 'fieldform'], 'required'],
+            [['nameform', 'fieldform'], 'string'],
+            [['count_upload_doc'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nameform' => 'Nameform',
+            'fieldform' => 'Fieldform',
+            'count_upload_doc' => 'Count Upload Doc',
+        ];
+    }
+
+    /**
+     * Gets query for [[Dataforms]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDataforms()
+    {
+        return $this->hasMany(Dataforms::class, ['fieldsforms_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[ExtraFields]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getExtraFields()
+    {
+        return $this->hasMany(ExtraFields::class, ['fieldsforms_id' => 'id']);
+    }
+}
